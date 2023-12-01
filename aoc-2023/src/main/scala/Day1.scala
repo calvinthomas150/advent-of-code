891ede22c7461e5b5a0fa89b0b0fee36ba48160c
Calvin Thomas <calvinthomas@protonmail.com>
2023-12-01 16:29:29 +0000

    Fixed test and switched to quiet syntax

--- aoc-2023/src/main/scala/Day1.scala
+++ aoc-2023/src/main/scala/Day1.scala
@@ -1,31 +1,27 @@
 object Day1 {
 
-  def run(mode:String, dayNumber: Int): Unit = {
+  def run(mode:String, dayNumber: Int): Unit =
     val linesPart1 = Utils.getInputLines(mode, dayNumber)
     part1(linesPart1)
     val linesPart2 = Utils.getInputLines(mode, dayNumber)
     part2(linesPart2)
-  }
 
-  def part1(lines: Iterator[String]): Unit = {
+  def part1(lines: Iterator[String]): Unit =
     val numbersOnly = removeAllNonNumericCharacters(lines)
     val firstAndLast = firstAndLastNumbers(numbersOnly)
     val result = combineAndSumFirstAndLast(firstAndLast)
     println(s"Part 1 Result: $result")
-  }
 
-  def part2(lines: Iterator[String]): Unit = {
+  def part2(lines: Iterator[String]): Unit =
     val numbersOnly = removeAllNonNumericWithWordsCharacters(lines)
     val firstAndLast = firstAndLastNumbers(numbersOnly)
     val result = combineAndSumFirstAndLast(firstAndLast)
     println(s"Part 2 Result: $result")
-  }
 
-  def removeAllNonNumericCharacters(lines:Iterator[String]):Iterator[String] = {
+  def removeAllNonNumericCharacters(lines:Iterator[String]):Iterator[String] =
     lines.map(_.replaceAll("[^0-9]", ""))
-  }
 
-  def removeAllNonNumericWithWordsCharacters(lines: Iterator[String]): Iterator[String] = {
+  def removeAllNonNumericWithWordsCharacters(lines: Iterator[String]): Iterator[String] =
       lines
         .map(_.replaceAll("one", "one1one")
               .replaceAll("two", "two2two")
@@ -37,15 +33,10 @@ object Day1 {
               .replaceAll("eight", "eight8eight")
               .replaceAll("nine", "nine9nine")
               .replaceAll("[^0-9]", ""))
-  }
 
-  def firstAndLastNumbers(numbersOnly: Iterator[String]): Iterator[(Int, Int)] = {
-    numbersOnly.map(n => {
-      (n.head.asDigit,n.last.asDigit)
-    })
-  }
+  def firstAndLastNumbers(numbersOnly: Iterator[String]): Iterator[(Int, Int)] =
+    numbersOnly.map(n => (n.head.asDigit,n.last.asDigit))
 
-  def combineAndSumFirstAndLast(nums: Iterator[(Int, Int)]): Int = {
+  def combineAndSumFirstAndLast(nums: Iterator[(Int, Int)]): Int =
     nums.map((x, y) => s"$x$y".toInt).sum
-  }
 }

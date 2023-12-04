35ef66a7d8e8394fb6d9e57ffc0da58d17f9ffa2
Calvin Thomas <calvin.thomas@turntabl.io>
2023-12-04 19:30:07 +0000

    Completed day 4 part 2

--- aoc-2023/src/main/scala/Day4.scala
+++ aoc-2023/src/main/scala/Day4.scala
@@ -1,22 +1,53 @@
 import Part.*
 
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 object Day4 {
 
-  case class Scratchcard(id: Int, winningNumbers: List[Int], cardNumbers: List[Int])
+  case class Scratchcard(id: Int, winningNumbers: List[Int], cardNumbers: List[Int], isCopy: Boolean = false)
 
   def run(mode: String, dayNumber: Int): Unit =
     val lines = Utils.getInputLines(mode, dayNumber).toList
     part1(lines)
+    part2(lines)
 
   def part1(lines: List[String]):BigInt =
     val scratchcards = getScratchcards(lines)
     val result = scratchcards.map(getPoints).sum
-    println(result)
     Utils.printResult(Part1, result.toString)
     result
 
+  def part2(lines: List[String]): Int =
+    val scratchcards = getScratchcards(lines).toList
+
+    val result = scratchcardMapWithCopies(scratchcards).values.sum
+
+    Utils.printResult(Part2, result.toString)
+    result
+
+
+  def scratchcardMapWithCopies(scratchcards: List[Scratchcard]): Map[Int, Int] =
+    val startingMap = (for(card <- scratchcards) yield card.id -> 1).toMap
+
+    val updatedMap =
+      scratchcards.foldLeft(startingMap)((currentMap, scratchcard) =>
+        val numberOfWinners = countWinningNumbers(scratchcard)
+        getUpdatedMap(scratchcard.id, numberOfWinners, currentMap))
+
+    updatedMap
+
+  def getUpdatedMap(id: Int, numberOfWinners: Int, currentMap: Map[Int, Int]): Map[Int, Int] =
+    val updated = (id + 1  to id + numberOfWinners).foldLeft(currentMap)((currentMap, updateId) =>
+      currentMap.updated(updateId, currentMap(id) + currentMap(updateId)))
+
+    updated
+
+  def countWinningNumbers(card: Scratchcard): Int =
+    (card.winningNumbers intersect card.cardNumbers).size
+
   def getPoints(card: Scratchcard):BigInt =
-    val winningNumbers = (card.winningNumbers intersect card.cardNumbers).size
+    val winningNumbers = countWinningNumbers(card: Scratchcard)
     BigDecimal.valueOf(Math.pow(2, winningNumbers - 1)).toBigInt
 
   def getScratchcards(lines: List[String]): Seq[Scratchcard] =
--- aoc-2023/src/test/scala/Day4Spec.scala
+++ aoc-2023/src/test/scala/Day4Spec.scala
@@ -17,6 +17,13 @@ class Day4Spec extends AnyFlatSpec with should.Matchers:
     val expected2 = List(83,86,6,31,17,9,48,53)
     Day4.convertNumbersInput(input2) should be(expected2)
 
+  "countWinningNumbers" should "return the count of the numbers in the card list that match the winning numbers" in :
+    val winningNumbers = List(41, 48, 83, 86, 17)
+    val cardNumbers = List(83, 86, 6, 31, 17, 9, 48, 53)
+    val scratchcard = Scratchcard(1, winningNumbers, cardNumbers)
+
+    Day4.countWinningNumbers(scratchcard) should be(4)
+
   "getPoints" should "return the correct number based on doubling for the amount of winning numbers matched" in:
     val winningNumbers = List(41,48,83,86,17)
     val cardNumbers = List(83,86,6,31,17,9,48,53)
@@ -34,3 +41,40 @@ class Day4Spec extends AnyFlatSpec with should.Matchers:
            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
 
     Day4.part1(input) should be(13)
+
+  "getUpdatedMap" should "update all the values in the map for the winner and return the updated map" in:
+    val numberOfWinners = 4
+    val map = Map(1 -> 1, 2 -> 1, 3 -> 1, 4 -> 1, 5 -> 1, 6 -> 1)
+    Day4.getUpdatedMap(1, numberOfWinners, map) should be(Map(1 -> 1, 2 -> 2, 3 -> 2, 4 -> 2, 5 -> 2, 6 -> 1))
+
+    val numberOfWinners2 = 2
+    val map2 = Map(1 -> 1, 2 -> 2, 3 -> 2, 4 -> 2, 5 -> 2, 6 -> 1)
+    Day4.getUpdatedMap(2, numberOfWinners2, map2) should be(Map(1 -> 1, 2 -> 2, 3 -> 4, 4 -> 4, 5 -> 2, 6 -> 1))
+
+  "scratchcardMapWithCopies" should "return a map containing all the copies of each scratchcard" in:
+
+    val scratchcards =
+      List(Scratchcard(1, List(41, 48, 83, 86, 17), List(83, 86, 6, 31, 17, 9, 48, 53)),
+           Scratchcard(2, List(13, 32, 20, 16, 61), List(61, 30, 68, 82, 17, 32, 24, 19)),
+           Scratchcard(3, List(1, 21, 53, 59, 44), List(69, 82, 63, 72, 16, 21, 14, 1)),
+           Scratchcard(4, List(41, 92, 73, 84, 69), List(59, 84, 76, 51, 58, 5, 54, 83)),
+           Scratchcard(5, List(87, 83, 26, 28, 32), List(88, 30, 70, 12, 93, 22, 82, 36)),
+           Scratchcard(6, List(31, 18, 13, 56, 72), List(74, 77, 10, 23, 35, 67, 36, 11)))
+
+    val expected = Map(5 -> 14, 1 -> 1, 6 -> 1, 2 -> 2, 3 -> 4, 4 -> 8)
+
+    Day4.scratchcardMapWithCopies(scratchcards) should be(expected)
+
+
+  "part2" should "return the count of all of the scratchcards including copies generated from winning" in:
+    val input =
+      List("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
+
+    Day4.part2(input) should be(30)
+
+

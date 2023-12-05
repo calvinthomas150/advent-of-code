714b7b0f4b8aecd25ebe54832f9668ebc2997853
Calvin Thomas <calvin.thomas@turntabl.io>
2023-12-05 09:29:20 +0000

    Added day 5 basic boilerplate

---
+++ aoc-2023/src/main/scala/Day5.scala
@@ -0,0 +1,15 @@
+case class MapRange(destinationStart:Long, sourceStart: Long, length: Long)
+
+
+object Day5:
+  def run(mode: String, dayNumber: Int): Unit =
+    val lines = Utils.getInputLines(mode, dayNumber).toList
+    part1(lines)
+    part2(lines)
+
+  def part1(lines: List[String]) =
+    ???
+
+  def part2(lines: List[String]) =
+    ???
+
---
+++ aoc-2023/src/test/scala/Day5Spec.scala
@@ -0,0 +1,6 @@
+import Day4.{Scratchcard, part1}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class Day5Spec extends AnyFlatSpec with should.Matchers:
+  ???

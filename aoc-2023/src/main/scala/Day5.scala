7aeaff504b3b2d0d70ee65f56c2208b37923049b
Calvin Thomas <calvin.thomas@turntabl.io>
2023-12-09 21:58:38 +0000

    Completed day 5

--- aoc-2023/src/main/scala/Day5.scala
+++ aoc-2023/src/main/scala/Day5.scala
@@ -2,38 +2,43 @@ import Part.*
 
 object Day5:
 
-  case class MapRange(destinationStart:Long, sourceStart: Long, length: Long)
+  type Source = Long
+  type Destination = Long
 
-  val identifiers: Seq[String] = List(
-    "seed-to-soil map:",
-    "soil-to-fertilizer map:",
-    "fertilizer-to-water map:",
-    "water-to-light map:",
-    "light-to-temperature map:",
-    "temperature-to-humidity map:",
-    "humidity-to-location map:")
+  enum Kind:
+    case SEED
+    case SOIL
+    case FERTILIZER
+    case WATER
+    case LIGHT
+    case TEMPERATURE
+    case HUMIDITY
+    case LOCATION
+
+  case class Resource(start: Long, end: Long, kind: Kind)
+  case class Property(destStart: Destination, srcStart: Source, srcEnd: Source ,rangeLength: Long)
+  case class ResourceMap(from: Kind, to: Kind, properties: List[Property])
 
   def run(mode: String, dayNumber: Int): Unit =
     val lines = Utils.getInputLines(mode, dayNumber).toList
     part1(lines)
+    part2(lines)
 
   def part1(lines: List[String]): Long =
-    val seeds = getSeeds(lines)
-    val transformations = identifiers.map(getMap(lines, _))
-    val locations = transformations.foldLeft(seeds):
-      (currentList, map) => currentList.map(item => getNextLinkInChain(item, map))
-
-    val result = locations.min
+    val seeds = getSeedsNoRange(lines)
+    val maps = getResourceMap(lines)
+    val result = calculate(seeds, maps)
     Utils.printResult(Part1, result.toString)
     result
 
-  def getNextLinkInChain(input: Long, map: Seq[MapRange]): Long =
-    map
-      .find(s => s.sourceStart <= input && s.sourceStart + s.length >= input)
-      .map(s => input - s.sourceStart + s.destinationStart)
-      .getOrElse(input)
+  def part2(lines: List[String]): Long =
+    val seeds = getSeedsRange(lines)
+    val maps = getResourceMap(lines)
+    val result = calculate(seeds, maps)
+    Utils.printResult(Part2, result.toString)
+    result
 
-  def getSeeds(lines: List[String]): List[Long] =
+  def getSeedsData(lines: List[String]): List[Long] =
     lines
       .head
       .drop(7)
@@ -41,17 +46,105 @@ object Day5:
       .map(_.trim.toLong)
       .toList
 
-  def getMap(lines: List[String], identifier: String): Seq[MapRange] =
-    lines
+  def getSeedsNoRange(lines: List[String]): List[Resource] =
+    getSeedsData(lines)
+      .map: start =>
+        Resource(start = start, end = start, kind = Kind.SEED)
+
+  def getSeedsRange(lines: List[String]): List[Resource] =
+    getSeedsData(lines)
+      .grouped(2)
+      .map:
+        case List(start, length) =>
+          Resource(start = start, end = start + length - 1, kind = Kind.SEED)
+      .toList
+
+  def getResourceMap(lines: List[String]): List[ResourceMap] =
+
+    val identifierMap =
+      Map(
+        "seed-to-soil map:" -> (Kind.SEED, Kind.SOIL),
+        "soil-to-fertilizer map:" -> (Kind.SOIL, Kind.FERTILIZER),
+        "fertilizer-to-water map:" -> (Kind.FERTILIZER, Kind.WATER),
+        "water-to-light map:" -> (Kind.WATER, Kind.LIGHT),
+        "light-to-temperature map:" -> (Kind.LIGHT, Kind.TEMPERATURE),
+        "temperature-to-humidity map:" -> (Kind.TEMPERATURE, Kind.HUMIDITY),
+        "humidity-to-location map:" -> (Kind.HUMIDITY, Kind.LOCATION))
+
+    val identifiers: Seq[String] = List(
+        "seed-to-soil map:",
+        "soil-to-fertilizer map:",
+        "fertilizer-to-water map:",
+        "water-to-light map:",
+        "light-to-temperature map:",
+        "temperature-to-humidity map:",
+        "humidity-to-location map:")
+
+    def getProperties(lines:List[String], identifier: String): List[Property] =
+      lines
       .dropWhile(!_.matches(identifier))
       .drop(1)
       .takeWhile(_.nonEmpty)
       .map: line =>
         val Array(dest, source, length) = line.split(" ").map(_.trim.toLong)
-        MapRange(dest, source, length)
+        Property(dest, source, source + length - 1 ,length)
 
+    identifiers.map: identifier =>
+      val (from:Kind, to:Kind) = identifierMap(identifier)
+      ResourceMap(from, to, getProperties(lines, identifier).sortBy(_.srcStart))
+    .toList
 
-  
+  def findNextResource(resource: Resource, resourceMap: ResourceMap): Seq[Resource] =
+
+    def hasOverlaps(start: Long, end: Long, propertyStart: Long, propertyEnd: Long) =
+      start >= propertyStart && start <= propertyEnd
+      || end >= propertyStart && end <= propertyEnd
+      || start <= propertyStart && end >= propertyEnd
+
+    def getInsideRange(start: Long, end: Long, to: Kind ,prop: Property, overlaps: Boolean) =
+      if (overlaps) then
+        val delay = prop.destStart - prop.srcStart
+        Some(Resource(
+          Math.max(start, prop.srcStart) + delay,
+          Math.min(end, prop.srcEnd) + delay,
+          to
+        ))
+      else None
+
+    def getUnderRange(start: Long, end:Long ,prop: Property, to: Kind): Option[Resource] =
+      if(start < prop.srcStart)
+        Option(Resource(start, Math.min(prop.srcStart - 1, end), to))
+      else None
+
+    def getOverRange(start: Long, end: Long, prop: Property, to: Kind) =
+      if(end > prop.srcEnd) Some(Resource(Math.max(start, prop.srcEnd + 1), end, to))
+      else None
+
+    val (newResources, res) =
+      val initial = (Nil:List[Resource], Option(resource))
+      resourceMap.properties.foldLeft(initial) {
+        case ((acc, Some(resource)), prop) =>
+          val Resource(start, end, _) = resource
+
+          val overlaps = hasOverlaps(start, end, prop.srcStart, prop.srcEnd)
+          val inRange = getInsideRange(start, end, resourceMap.to, prop, overlaps)
+          val underRange = getUnderRange(start, end, prop, resourceMap.to)
+          val overRange = getOverRange(start, end, prop, resourceMap.to)
+          (List(underRange, inRange, acc).flatten, overRange)
+
+        case ((acc, None), _) => (acc, None)
+      }
+    List(newResources, res).flatten
+
+  def calculate(seeds: List[Resource], resourceMaps: List[ResourceMap]): Long =
+    def loop(resource: Resource): List[Resource] =
+      if (resource.kind == Kind.LOCATION) List(resource)
+      else
+        val resourceMap = resourceMaps.find(_.from == resource.kind).get
+        findNextResource(resource, resourceMap).flatMap(loop).toList
+
+    seeds.flatMap(loop).minBy(_.start).start
+
 
 
 
--- aoc-2023/src/test/scala/Day5Spec.scala
+++ aoc-2023/src/test/scala/Day5Spec.scala
@@ -1,17 +1,26 @@
-import Day4.{Scratchcard, part1}
-import Day5.MapRange
+import Day5.{Property, ResourceMap}
+import Day5.Kind.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+
 
 class Day5Spec extends AnyFlatSpec with should.Matchers:
   "getSeeds" should "return a list of numbers as the seeds" in:
     val lines:List[String] = List("seeds: 79 14 55 13", "", "seed - to - soil map:")
-    Day5.getSeeds(lines) should be(List(79,14,55,13))
+    Day5.getSeedsData(lines) should be(List(79,14,55,13))
 
-  "getMap" should "return a list of MapRange objects for the particular map" in:
+  "getResources" should "return a list of the resource maps" in:
     val lines:List[String] = Utils.getInputLines("test", 5).toList
-    Day5.getMap(lines, "seed-to-soil map:") should be(List(MapRange(50, 98, 2), MapRange(52, 50, 48)))
-    Day5.getMap(lines, "soil-to-fertilizer map:") should be(List(MapRange(0, 15, 37), MapRange(37, 52, 2), MapRange(39, 0, 15)))
+    val expected =
+      List(ResourceMap(SEED, SOIL, List(Property(52, 50, 97, 48), Property(50, 98, 99, 2))),
+        ResourceMap(SOIL, FERTILIZER, List(Property(39, 0, 14, 15), Property(0, 15, 51, 37), Property(37, 52, 53, 2))),
+        ResourceMap(FERTILIZER, WATER, List(Property(42, 0, 6, 7), Property(57, 7, 10, 4), Property(0, 11, 52, 42), Property(49, 53, 60, 8))),
+        ResourceMap(WATER, LIGHT, List(Property(88, 18, 24, 7), Property(18, 25, 94, 70))),
+        ResourceMap(LIGHT, TEMPERATURE, List(Property(81, 45, 63, 19), Property(68, 64, 76, 13), Property(45, 77, 99, 23))),
+        ResourceMap(TEMPERATURE, HUMIDITY, List(Property(1, 0, 68, 69), Property(0, 69, 69, 1))),
+        ResourceMap(HUMIDITY, LOCATION, List(Property(60, 56, 92, 37), Property(56, 93, 96, 4))))
+
+    Day5.getResourceMap(lines) should be(expected)
 
   "part1" should "return the correct value" in:
     val lines = Utils.getInputLines("test", 5).toList

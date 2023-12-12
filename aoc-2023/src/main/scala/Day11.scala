f51fea8f90c5c745b771a55d14bb20268aeccd54
Calvin Thomas <calvin.thomas@turntabl.io>
2023-12-12 20:08:09 +0000

    Added basic set-up for day

---
+++ aoc-2023/src/main/scala/Day11.scala
@@ -0,0 +1,14 @@
+object Day11 {
+
+  def run(mode: String, dayNumber: Int): Unit =
+    val lines = Utils.getInputLines(mode, dayNumber).toList
+    part1(lines)
+    part2(lines)
+
+  def part1(lines: List[String]): Int =
+    ???
+    
+  def part2(lines: List[String]): Int =
+    ???
+
+}
---
+++ aoc-2023/src/test/scala/Day11Spec.scala
@@ -0,0 +1,5 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class Day11Spec extends AnyFlatSpec with should.Matchers:
+  ???

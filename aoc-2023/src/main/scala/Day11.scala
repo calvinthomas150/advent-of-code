3e7a64960c2982d1e2a369d9c0d90895b21b32b7
Calvin Thomas <calvin.thomas@turntabl.io>
2023-12-22 23:56:33 +0000

    Completed day 11 part 1

--- aoc-2023/src/main/scala/Day11.scala
+++ aoc-2023/src/main/scala/Day11.scala
@@ -1,4 +1,9 @@
-object Day11 {
+import Part.Part1
+
+object Day11:
+
+  type Universe = List[String]
+  type Coordinate = (Int, Int)
 
   def run(mode: String, dayNumber: Int): Unit =
     val lines = Utils.getInputLines(mode, dayNumber).toList
@@ -6,9 +11,49 @@ object Day11 {
     part2(lines)
 
   def part1(lines: List[String]): Int =
-    ???
-    
+    val result = run(lines, 2)
+    Utils.printResult(Part1, result.toString)
+    result
+
   def part2(lines: List[String]): Int =
     ???
+  
+  def run(lines: List[String], factor: Int): Int =
+    given expansionFactor:Int = factor
+    val universe = parse(lines)
+    val galaxyCoordinates = getGalaxyCoordinates(universe)
+    val galaxyPairs = getGalaxyPairs(galaxyCoordinates)
+    galaxyPairs.map((c1, c2) => calculateCoordinateDistance(c1, c2)).sum
 
-}
+  def getGalaxyPairs(coordinates: Seq[Coordinate]):Seq[(Coordinate, Coordinate)] =
+    for(c1 <- coordinates.indices;
+        c2 <- coordinates.indices if c1 < c2) yield (coordinates(c1), coordinates(c2))
+
+  def getGalaxyCoordinates(universe: Universe): Seq[Coordinate] =
+    for(x <- universe.indices;
+         y <- universe(x).indices if universe(x)(y) == '#') yield (x, y)
+
+  def calculateCoordinateDistance(first: Coordinate, second: Coordinate): Int =
+    val (firstX, firstY) = first
+    val (secondX, secondY) = second
+
+    (firstX - secondX).abs + (firstY - secondY).abs
+
+  def addExtraRows(lines: List[String])(using expansionFactor: Int): List[String] =
+    lines.foldLeft(List.empty[String])((acc, line) =>
+      if(line.forall(_ == '.'))
+        List.fill(expansionFactor)(line) ++ acc
+      else line :: acc
+    ).reverse
+
+  def transposeLines(lines: List[String]): List[String] =
+    lines
+      .map(_.toList)
+      .transpose
+      .map(_.mkString(""))
+
+  def addExtraColumns(lines: List[String])(using expansionFactor: Int): List[String] =
+    transposeLines(addExtraRows(transposeLines(lines)))
+
+  def parse(lines: List[String])(using expansionFactor: Int): Universe =
+    addExtraColumns(addExtraRows(lines))
--- aoc-2023/src/test/scala/Day11Spec.scala
+++ aoc-2023/src/test/scala/Day11Spec.scala
@@ -2,4 +2,86 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class Day11Spec extends AnyFlatSpec with should.Matchers:
-  ???
+
+  given expansionCount: Int = 2
+  
+  "addExtraRows" should "add extra rows where there are no galaxies" in:
+    val input = Utils.getInputLines("test", 11).toList
+    val expected =
+      List( "...#......",
+            ".......#..",
+            "#.........",
+            "..........",
+            "..........",
+            "......#...",
+            ".#........",
+            ".........#",
+            "..........",
+            "..........",
+            ".......#..",
+            "#...#.....")
+
+    Day11.addExtraRows(input) should be(expected)
+
+  "addExtraColumns" should "add extra columns where there are no galaxies" in :
+    val input = Utils.getInputLines("test", 11).toList
+    val expected =
+      List( "....#........",
+            ".........#...",
+            "#............",
+            ".............",
+            "........#....",
+            ".#...........",
+            "............#",
+            ".............",
+            ".........#...",
+            "#....#.......")
+
+    Day11.addExtraColumns(input) should be(expected)
+
+  "parse" should "add extra rows and columns where there are no galaxies" in:
+    val input = Utils.getInputLines("test", 11).toList
+    val expected =
+      List( "....#........",
+            ".........#...",
+            "#............",
+            ".............",
+            ".............",
+            "........#....",
+            ".#...........",
+            "............#",
+            ".............",
+            ".............",
+            ".........#...",
+            "#....#.......")
+
+    Day11.parse(input) should be(expected)
+
+  "getGalaxyCoordingates" should "return a List of all the coordingates of galaxies" in:
+    val input = Utils.getInputLines("test", 11).toList
+    val universe = Day11.parse(input)
+    Day11.getGalaxyCoordinates(universe) should be (List((0, 4), (1, 9), (2, 0), (5, 8), (6, 1), (7, 12), (10, 9), (11, 0), (11, 5)))
+
+  "calculateCoordinateDistance" should "return the correct distance between two galaxies" in:
+    val first = (6,1)
+    val second = (11, 5)
+    Day11.calculateCoordinateDistance(first, second) should be(9)
+
+    val first2 = (0,4)
+    val second2 = (10, 9)
+    Day11.calculateCoordinateDistance(first2, second2) should be (15)
+    
+  "getGalaxyPairs" should "return a sequence of all coordinate pairs, pairs should be treated the same if they're in the opposite order, i.e. (2,1) == (1,2)" in:
+    val coordinates = List((1,2), (3,4), (5,6), (7,8))
+    val pairs = List(((1,2), (3,4)), ((1,2), (5,6)), ((1,2), (7,8)), ((3,4), (5,6)), ((3,4), (7,8)),((5,6), (7,8)))
+    
+    Day11.getGalaxyPairs(coordinates) should be(pairs)
+    
+  "part1" should "calculate the shortest path between all pairs of galaxies" in:
+    val input = Utils.getInputLines("test", 11).toList
+    Day11.part1(input) should be(374)
+
+  "part2" should "calculate the shortest path between all pairs of galaxies with larger expansion" in :
+    val input = Utils.getInputLines("test", 11).toList
+    Day11.part2(input) should be(374)
+
